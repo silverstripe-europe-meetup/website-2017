f0c899939c140b2e8685d0ba74ab3f4faafbe8ae
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2017-08-22 19:06:29 +0200

    Very basic SS3.6.1 installation with deployer 5

---
+++ mysite/_config.php
@@ -0,0 +1,14 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = '';
+
+require_once 'conf/ConfigureFromEnv.php';
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+HtmlEditorConfig::get('cms')->insertButtonsAfter('visualaid', 'removeformat');
---
+++ mysite/code/Page.php
@@ -0,0 +1,37 @@
+<?php
+
+class Page extends SiteTree {
+
+
+}
+class Page_Controller extends ContentController {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array (
+	);
+
+	public function init() {
+		parent::init();
+		// You can include any CSS or JS required by your project here.
+		// See: http://doc.silverstripe.org/framework/en/reference/requirements
+
+		if(Member::currentUserID() || Director::isDev()) {
+			SS_Cache::set_cache_lifetime('any', -1, 100);
+		}
+	}
+
+}
